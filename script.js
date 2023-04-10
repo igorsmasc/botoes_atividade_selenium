1a2f8ebb59c7051dfcb4ef5ce279aa42f353361a
Igor Santos Mascarenhas <46729174+igorsmasc@users.noreply.github.com>
2023-04-10 00:17:50 -0300

    Improve code examples and add more cases

--- script.js
+++ script.js
@@ -1,31 +1,39 @@
-$(document).ready(function() {
-    // Ao clicar no Botão 1
-    $("#button1").click(function() {
-      alert("O botão 1 foi selecionado.");
-    });
-  
-    // Ao clicar no Botão 2
-    $("#button2").click(function() {
-      alert("O botão 2 foi selecionado.");
-    });
-  
-    // Ao clicar no Botão 3
-    $("#button3").click(function() {
-      alert("O botão 3 foi selecionado.");
-    });
-  
-    // Ao clicar no Botão 4
-    $("#button4").click(function() {
-      alert("O botão 4 foi selecionado.");
-    });
-  
-    // Ao clicar no switch para habilitar/desabilitar o Botão 4
-    $("#button4-switch").change(function() {
-      if ($(this).is(":checked")) {
-        $("#button4").prop("disabled", false);
-      } else {
-        $("#button4").prop("disabled", true);
-      }
-    });
+$(document).ready(function () {
+  // Ao clicar no Botão 1
+  $("#button1").click(function () {
+    alert("O botão 1 foi selecionado.");
   });
-  
+
+  // Ao clicar no Botão 2
+  $("#button2").click(function () {
+    alert("O botão 2 foi selecionado.");
+  });
+
+  // Ao clicar no Botão 3
+  $("#button3").click(function () {
+    alert("O botão 3 foi selecionado.");
+  });
+
+  // Ao clicar no Botão 4
+  $("#button4").click(function () {
+    alert("O botão 4 foi selecionado.");
+  });
+
+  // Ao clicar no Botão 5
+  $("#button5").click(function () {
+    alert("O botão 5 foi selecionado.");
+  });
+
+  $("#button6").click(function () {
+    alert("O botão 6 foi selecionado.");
+  });
+
+  // Ao clicar no switch para habilitar/desabilitar o Botão 6
+  $("#button6-switch").change(function () {
+    if ($(this).is(":checked")) {
+      $("#button6").prop("disabled", false);
+    } else {
+      $("#button6").prop("disabled", true);
+    }
+  });
+});
